9ea57f27d026d827df12921964e6d561c76a88df
Theodore Roley <tvroley@uw.edu>
2024-02-14 00:57:48 -0800

    prop-types

--- trading-cards/src/TradingCard.jsx
+++ trading-cards/src/TradingCard.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
 function TradingCard({tradingCard}) {
   const [card, setCard] = useState(tradingCard);
+  
   return (
     <div key={card.certificationNumber} className='div-card'>
       <img src={card.frontCardImageLink} alt={`picture of a ${card.player} card`}/>
     </div>
   )
+}
+
+TradingCard.propTypes = {
+    tradingCard: PropTypes.object.isRequired
 }
 
 export default TradingCard;
