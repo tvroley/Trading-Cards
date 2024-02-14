a061bc5e11172a26ed35bbb611829173fd786565
Theodore Roley <tvroley@uw.edu>
2024-02-14 01:53:04 -0800

    clicking card image flips card over

--- trading-cards/src/TradingCard.jsx
+++ trading-cards/src/TradingCard.jsx
@@ -4,10 +4,19 @@ import PropTypes from 'prop-types';
 
 function TradingCard({tradingCard}) {
   const [card, setCard] = useState(tradingCard);
+
+  const flipImage = (event) => {
+    const el = event.target;
+    if(el.src === card.frontCardImageLink) {
+        el.src = card.backCardImageLink;
+    } else {
+        el.src = card.frontCardImageLink;
+    }
+  }
   
   return (
       <>
-        <img src={card.frontCardImageLink} alt={`picture of a ${card.player} card`}/>
+        <img src={card.frontCardImageLink} alt={`picture of a ${card.player} card`} onClick={(event) => flipImage(event)}/>
         <p>{card.certificationNumber}</p>
       </>
   )
