8958c363262bd5aed2c47f44c4bbdf453baa5038
Theodore Roley <tvroley@uw.edu>
2024-02-19 23:03:48 -0800

    collection buttons for home page

--- trading-cards/src/App.jsx
+++ trading-cards/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   
   return (
     <div>
+      <h1>Your Collection</h1>
       <Nav/>
       <Routes>
         <Route path="/" element={<Home/>} />
--- trading-cards/src/Home.jsx
+++ trading-cards/src/Home.jsx
@@ -1,10 +1,20 @@
-import Nav from "./Nav";
-import { Outlet } from "react-router";
+import {useNavigate} from "react-router-dom";
 
 export default function Home() {
+    const navigate = useNavigate();
+
+    const goGrandpa = () => {
+        navigate('/grandpa');
+    }
+
+    const goUncle = () => {
+        navigate('/uncle');
+    }
+
     return (
-        <div>
-          <h1>Your Collection</h1>
+        <div className="div-cards-buttons">
+          <button onClick={goGrandpa}>Grandpa's Collection</button>
+          <button onClick={goUncle}>Uncle's Collection</button>
         </div>
     );
 }
