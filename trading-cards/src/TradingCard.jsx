28315051201763813be8dd16d331b8b218919f6c
Theodore Roley <tvroley@uw.edu>
2024-02-19 21:45:30 -0800

    button toggles between big and small images

--- trading-cards/src/TradingCard.jsx
+++ trading-cards/src/TradingCard.jsx
@@ -22,14 +22,20 @@ function TradingCard({tradingCard, index, tradingCardCollection, setTradingCardC
     setTradingCardCollection([...arr1, cardClone, ...arr2]);
     setCard(cardClone);
   }
+
+  const toggleImageSize = (event) => {
+    const el = event.target.parentElement.firstChild;
+    console.log(el.classList.toggle('img-small'));
+  }
   
   return (
       <>
-        <img src={card.frontCardImageLink} alt={`picture of a ${card.player} card`} onClick={(event) => flipImage(event)}/>
+        <img src={card.frontCardImageLink} alt={`picture of a ${card.player} card`} onClick={(event) => flipImage(event)} className='img-small'/>
         <p>{card.cardSet} #{card.cardNumber} {card.player}</p>
         <p>{card.gradingCompany} {card.grade} #{card.certificationNumber}</p>
         <label htmlFor=''>Sold</label>
         <input type='checkbox' checked={card.sold} onChange={(event) => toggleSold(event)} id={`${card.certificationNumber}soldCheckbox`} />
+        <button onClick={(event) => toggleImageSize(event)}>Make Photo Big/Small</button>
       </>
   )
 }
