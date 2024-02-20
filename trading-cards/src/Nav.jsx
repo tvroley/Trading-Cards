b95946c1590cbf49689f024bd30e00e39ef9666d
Theodore Roley <tvroley@uw.edu>
2024-02-19 19:52:44 -0800

    put links in navigation bar

--- trading-cards/src/Nav.jsx
+++ trading-cards/src/Nav.jsx
@@ -2,12 +2,10 @@ import { Link } from 'react-router-dom';
 
 export default function Nav() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/grandpa">Grandpa's Collection</Link></li>
-          <li><Link to="/uncle">Uncle's Collection</Link></li>
-        </ul>
+      <div className='div-navigation'>
+        <Link to="/">Home</Link>
+        <Link to="/grandpa">Grandpa's Collection</Link>
+        <Link to="/uncle">Uncle's Collection</Link>
       </div>
     );
 }
