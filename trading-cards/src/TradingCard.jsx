16f1c4f45f78d31ecd527344d52c0d00184213c9
Theodore Roley <tvroley@uw.edu>
2024-02-19 22:03:05 -0800

    swapped flip image and enlarge photo buttons

--- trading-cards/src/TradingCard.jsx
+++ trading-cards/src/TradingCard.jsx
@@ -6,7 +6,7 @@ function TradingCard({tradingCard, index, tradingCardCollection, setTradingCardC
   const [card, setCard] = useState(tradingCard);
 
   const flipImage = (event) => {
-    const el = event.target;
+    const el = event.target.parentElement.parentElement.firstChild;
     if(el.src === card.frontCardImageLink) {
         el.src = card.backCardImageLink;
     } else {
@@ -24,18 +24,22 @@ function TradingCard({tradingCard, index, tradingCardCollection, setTradingCardC
   }
 
   const toggleImageSize = (event) => {
-    const el = event.target.parentElement.firstChild;
+    const el = event.target;
     console.log(el.classList.toggle('img-small'));
   }
   
   return (
       <>
-        <img src={card.frontCardImageLink} alt={`picture of a ${card.player} card`} onClick={(event) => flipImage(event)} className='img-small'/>
+        <img src={card.frontCardImageLink} alt={`picture of a ${card.player} card`} onClick={(event) => toggleImageSize(event)} className='img-small'/>
         <p>{card.cardSet} #{card.cardNumber} {card.player}</p>
         <p>{card.gradingCompany} {card.grade} #{card.certificationNumber}</p>
-        <label htmlFor=''>Sold</label>
-        <input type='checkbox' checked={card.sold} onChange={(event) => toggleSold(event)} id={`${card.certificationNumber}soldCheckbox`} />
-        <button onClick={(event) => toggleImageSize(event)}>Make Photo Big/Small</button>
+        <div className='div-cards-buttons'>
+          <div>
+            <label htmlFor={`${card.certificationNumber}soldCheckbox`}>Sold</label>
+            <input type='checkbox' checked={card.sold} onChange={(event) => toggleSold(event)} id={`${card.certificationNumber}soldCheckbox`} />
+          </div>
+          <button onClick={(event) => flipImage(event)}>Flip Image</button>
+        </div>
       </>
   )
 }
